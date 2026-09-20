f271914986104fd728a4d4b68265a8e654886daf
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-20 16:57:02 +0000

    chore: organize auth module imports

--- apps/api/src/modules/v1/auth/auth.module.ts
+++ apps/api/src/modules/v1/auth/auth.module.ts
@@ -21,8 +21,8 @@ import { AuthCredentialService } from './services/auth-credential.service';
 import { AuthEmailVerificationService } from './services/auth-email-verification.service';
 import { AuthOAuthService } from './services/auth-oauth.service';
 import { AuthPasswordService } from './services/auth-password.service';
-import { AuthSessionCleanupService } from './services/auth-session-cleanup.service';
 import { AuthSessionService } from './services/auth-session.service';
+import { AuthSessionCleanupService } from './services/auth-session-cleanup.service';
 import { GoogleStrategy, JwtStrategy, LocalStrategy } from './strategies';
 
 @Module({
