5acc8122ec056d4cc93071d74e5883977e719844
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-20 20:33:46 +0000

    chore: register webhook processing migration

--- apps/api/src/common/database/migrations/index.ts
+++ apps/api/src/common/database/migrations/index.ts
@@ -85,3 +85,5 @@ export * from './1788205216357-add-position-deleted-at';
 export * from './1788205216358-add-leave-balance-deleted-at';
 export * from './1788305216359-PayrollRunUniquePerCurrency';
 export * from './1788305216360-payroll-webhook-events';
+
+export * from './1789930060361-payroll-webhook-processing';
